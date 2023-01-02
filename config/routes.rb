6e45fa7315b9250d995967ba615fb475b2f1f8c5
ian <ianfletcher@Ians-MBP-2.attlocal.net>
2023-01-02 15:45:02 -0600

    added root

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root "scores#index"
   resources :scores
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # get "/scores", to: "scores#index"
