c09b938661e979eeda09ba923a865c7eb6a282be
ian <ianfletcher@Ians-MBP-2.attlocal.net>
2023-01-02 14:48:19 -0600

    added scores show

--- app/controllers/scores_controller.rb
+++ app/controllers/scores_controller.rb
@@ -1,2 +1,13 @@
 class ScoresController < ApplicationController
+  before_action :get_score, except: [:index]
+  def index
+    @scores = Score.all
+  end 
+
+  def show
+  end
+  private
+  def get_score
+    @score = Score.find(params[:id])
+  end
 end
